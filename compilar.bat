c3b183ee2e9d950bbf8875d1f70239a0370ad276
Abdiel352 <abdiel.abadil@gmail.com>
2026-05-09 19:09:50 -0400

    cleanup: deleta arquivos de compilação

--- compilar.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Compilando o Navegador...
-gcc -I./include src/main.c src/lista.c src/storage.c src/ui.c src/logica.c -o navegador.exe
-if %errorlevel% equ 0 (
-    echo Compilado com sucesso! Rode .\navegador.exe
-) else (
-    echo Erro na compilacao.
-)
-pause
